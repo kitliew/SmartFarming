d7e0e7af74ac658cbe7e8719f7770d3b314350e3
kitliew <kit.liew@outlook.com>
2020-09-22 17:58:28 +0800

    db_script.py can be used as import module@

--- db_script.py
+++ db_script.py
@@ -4,43 +4,38 @@
 # This script contain two part
 #   1. Will create table if device/table_name not in database
 #   2. Insert value to database
-# need import module/write a script to transfer Arduino converted readings
 
-# arrange/assign class to code. Potential to use multiple Arduino for one RPI0W
 
-import sys
+import time
 import mysql.connector
-#TODO import arduino_converted_reading
 
-# global variable
-host="192.168.0.142"    # IP address hosting the MySQL
-user="smartfarming"   # MySQL username
-password="smartfarming"   # MySQL password
-database="SmartFarming"   # MySQL database name
+# current date and time
+date_now, time_now = time.strftime('%Y-%m-%d %H:%M:%S').split()
 
-# establish connection (SmartFarming database/mysql)
-mydb = mysql.connector.connect(
-  host=host,  # IP address hosting the MySQL
-  user=user,  # MySQL username
-  password=password,  # MySQL pass
-  database=database
-)
 
 class Farmer(object):
+    # global variable
+    host = "192.168.0.142"  # IP address hosting the MySQL
+    user = "smartfarming"  # MySQL username
+    password = "smartfarming"  # MySQL password
+    database = "SmartFarming"  # MySQL database name
 
-  host = host
-  user = user
-  password = password
-  database = database
+    # establish connection (SmartFarming database/mysql)
+    mydb = mysql.connector.connect(
+        host=host,  # IP address hosting the MySQL
+        user=user,  # MySQL username
+        password=password,  # MySQL pass
+        database=database
+    )
 
-  def __init__(self, device):
-    self.farmer = device    # will be used as table name
+    def __init__(self, device):
+        self.farmer = device  # will be used as table name
 
-    # creating a cursor object using the cursor() method
-    mycursor = mydb.cursor()
+        # creating a cursor object using the cursor() method
+        mycursor = self.mydb.cursor()
 
-    # preparing SQL query to CREATE TABLE into the database.
-    sql = """CREATE TABLE IF NOT EXISTS `{}` (
+        # preparing SQL query to CREATE TABLE into the database.
+        sql = """CREATE TABLE IF NOT EXISTS `{}` (
     Device TEXT,
     Date DATE,
     Time TIME,
@@ -51,27 +46,31 @@ class Farmer(object):
     );
     """.format(self.farmer)
 
-    # execute the SQL command
-    mycursor.execute(sql)
+        # execute the SQL command
+        mycursor.execute(sql)
+        # log/report table name created in database.
+        print("{} table created in MySQL {} database".format(self.farmer, self.database))
 
-    #TODO should i close the cursor?
+    def insert_meter_values(self, sensors):
+        # SQL query to INSERT VALUES into table
+        sensor_sql = """INSERT INTO `{}` VALUES {};""".format(self.farmer, sensors)
 
-  def insert_meter_values(self, sensors):
-    # SQL query to INSERT VALUES into table
-    sensor_sql = """INSERT INTO `{}` VALUES (%s);""".format(self.farmer) # TODO: add sensors variable
+        # creating a cursor object using the cursor() method
+        mycursor = self.mydb.cursor()
 
-    # creating a cursor object using the cursor() method
-    mycursor = mydb.cursor()
+        try:
+            # execute the SQL command
+            mycursor.execute(sensor_sql)
 
-    try:
-      # execute the SQL command
-      mycursor.execute(sensor_sql, sensors)
+            # commit changes in database
+            self.mydb.commit()
+            print("Commit sensors reading {}".format(sensors))
 
-      # commit changes in database
-      mydb.commit()
+        except:
+            # Rolling back in case of error
+            self.mydb.rollback()
+            print("error, rollback")
 
-    except:
-      # Rolling back in case of error
-      mydb.rollback()
-
-    mydb.close()
+        # Do not close connection here. Will terminate other connection
+        # self.mydb.close()
+        # print("CLOSED")
