d6178f4b872409841391021d6aab7dbe9a5dd51e
kitliew <kit.liew@outlook.com>
2020-10-22 17:41:36 +0800

    test flask

--- WebServer/appSmartFarming.py
+++ WebServer/appSmartFarming.py
@@ -1,11 +1,10 @@
+from flask import Flask, render_template, request
 import mysql.connector
 import time
 
 #TODO get data from mysql
 #TODO graph dot plot
 #TODO live update?
-
-
 
 host = "192.168.0.142"  # IP address hosting the MySQL
 user = "smartfarming"  # MySQL username
@@ -24,14 +23,30 @@ mycursor = mydb.cursor()
 
 mycursor.execute("USE SmartFarming")
 
-
 def last_data():
     sql = "SELECT * FROM JOE ORDER BY DateTime DESC LIMIT 1;"
     mycursor.execute(sql)
     for i in mycursor:
         # return rows:
-        # device, dt, ec, humidity, ph, temperature = i
+        # name, dt, ec, humidity, ph, temperature = i
         return i
 
-name, dt, ec, humidity, ph, temperature = last_data()
-print(dt)
+
+app = Flask(__name__)
+
+#main route
+@app.route("/")
+def index():
+    device, date_time, ec, humidity, ph, temperature = last_data()
+    templateData = {
+        'device': device,
+        'date_time': date_time,
+        'ec': ec,
+        'humidity': humidity,
+        'ph': ph,
+        'temperature': temperature
+    }
+    return render_template("index.html", **templateData)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=80)
